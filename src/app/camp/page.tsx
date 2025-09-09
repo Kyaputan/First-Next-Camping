12812faed5a375263701158edd01ece3c4b7419f
Rachata Singkhet <161452629+Kyaputan@users.noreply.github.com>
2025-09-09 11:57:51 +0700

    Page

---
+++ src/app/camp/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+        <p>Camp</p>
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/favorite/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div>
+        <p>Favorite</p>
+    </div>
+  )
+}
+
+export default page
